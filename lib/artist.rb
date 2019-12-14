a826699e7d5a72206b9138c3cca4edf6cb95722f
Ashley Norton <nortonashleym@gmail.com>
2019-12-14 22:37:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,15 +14,11 @@ class Artist
   end 
   
   def add_song(song)
+    song = Song.new(song)
     song.artist = self 
     @songs << song 
   end 
   
-  def songs
-    Song.all.select { |song|
-      song.artist = self 
-    }
-  end 
   
  
 end 
