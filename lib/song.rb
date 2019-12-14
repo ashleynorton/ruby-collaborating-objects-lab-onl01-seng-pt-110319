e890426790af2e082fd1a0fa26c598cf6568ed7e
Ashley Norton <nortonashleym@gmail.com>
2019-12-14 22:55:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,9 +12,10 @@ class Song
     @@all 
   end 
   
-  def self.new_by_filename(file_name)
-    song
-    
+  def self.new_by_filename(filename)
+    song = self.new 
+    song.title = filename.split(" - ")[1]
+    song 
   end 
   
 end 
